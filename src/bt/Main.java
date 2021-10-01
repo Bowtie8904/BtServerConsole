a5d8b51ca8c40e0cc6e8d4f4934480b33f9649af
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2021-10-01 14:15:39 +0200

    Updated version. Setup logger.

--- src/bt/Main.java
+++ src/bt/Main.java
@@ -1,124 +1,123 @@
 package bt;
 
-import java.io.IOException;
-import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
-
 import bt.console.input.ArgumentParser;
 import bt.console.input.FlagArgument;
 import bt.console.input.ValueArgument;
 import bt.console.output.styled.Style;
+import bt.log.Logger;
 import bt.remote.socket.MulticastClient;
 import bt.remote.socket.evnt.mcast.MulticastClientKilled;
 import bt.remote.socket.evnt.mcast.MulticastClientStarted;
 import bt.scheduler.Threads;
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author &#8904
- *
  */
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        try
+        Logger.global().hookSystemErr();
+        Logger.global().hookSystemOut();
+        Logger.global().setLogToFile(false);
+        Logger.global().setPrintCaller(false);
+        Logger.global().setPrintTimestamp(false);
+
+        var parser = new ArgumentParser("-");
+
+        var discoverCmd = new FlagArgument("discover", "d").usage("-discover")
+                                                           .description("Attempts to discover possible servers to connect to.");
+
+        var hostCmd = new ValueArgument("host", "h").usage("-host <hostname>")
+                                                    .description("[Optional] Sets the hostname to connect to. Ommit to connect to localhost.");
+
+        var portCmd = new ValueArgument("port", "p").usage("-port <portnumber>")
+                                                    .description("Sets the port to connect to or the port to listen on if the server flag is used.");
+
+        var rawCmd = new FlagArgument("raw", "r").usage("-raw")
+                                                 .description("[Optional] Sets the type of client to raw to send and receive raw byte data.");
+
+        var serverCmd = new FlagArgument("server", "s").usage("-server")
+                                                       .description("[Optional] Indicates that a server should be created instead of a client.");
+
+        parser.register(discoverCmd);
+        parser.register(hostCmd);
+        parser.register(portCmd);
+        parser.register(rawCmd);
+        parser.register(serverCmd);
+        parser.registerDefaultHelpArgument();
+        parser.registerDefaultStyleArgument();
+        parser.parse(args);
+
+        if (discoverCmd.isExecuted())
         {
-            var parser = new ArgumentParser("-");
+            var client = new MulticastClient(MulticastClient.DEFAULT_PORT, MulticastClient.DEFAULT_GROUP_ADDRESS);
+            client.getEventDispatcher().subscribeTo(MulticastClientStarted.class, e -> printMessage("Started multicast client %s:%s",
+                                                                                                    formatHostPort()));
 
-            var discoverCmd = new FlagArgument("discover", "d").usage("-discover")
-                                                               .description("Attempts to discover possible servers to connect to.");
+            client.getEventDispatcher().subscribeTo(MulticastClientKilled.class, e -> printMessage("Killed multicast client %s:%s",
+                                                                                                   formatHostPort()));
 
-            var hostCmd = new ValueArgument("host", "h").usage("-host <hostname>")
-                                                        .description("[Optional] Sets the hostname to connect to. Ommit to connect to localhost.");
+            client.onMulticastReceive(packet ->
+                                      {
+                                          String message = new String(packet.getData(), 0, packet.getLength());
 
-            var portCmd = new ValueArgument("port", "p").usage("-port <portnumber>")
-                                                        .description("Sets the port to connect to or the port to listen on if the server flag is used.");
-
-            var rawCmd = new FlagArgument("raw", "r").usage("-raw")
-                                                     .description("[Optional] Sets the type of client to raw to send and receive raw byte data.");
-
-            var serverCmd = new FlagArgument("server", "s").usage("-server")
-                                                           .description("[Optional] Indicates that a server should be created instead of a client.");
-
-            parser.register(discoverCmd);
-            parser.register(hostCmd);
-            parser.register(portCmd);
-            parser.register(rawCmd);
-            parser.register(serverCmd);
-            parser.registerDefaultHelpArgument("help", "h");
-            parser.parse(args);
-
-            if (discoverCmd.isExecuted())
-            {
-                var client = new MulticastClient(MulticastClient.DEFAULT_PORT, MulticastClient.DEFAULT_GROUP_ADDRESS);
-                client.getEventDispatcher().subscribeTo(MulticastClientStarted.class, e -> printMessage("Started multicast client %s:%s",
-                                                                                                        formatHostPort()));
-
-                client.getEventDispatcher().subscribeTo(MulticastClientKilled.class, e -> printMessage("Killed multicast client %s:%s",
-                                                                                                       formatHostPort()));
-
-                client.onMulticastReceive(packet ->
+                                          if (!message.trim().equalsIgnoreCase("discover"))
                                           {
-                                              String message = new String(packet.getData(), 0, packet.getLength());
+                                              System.out.println(Style.apply(message, "yellow"));
+                                          }
+                                      });
 
-                                              if (!message.trim().equalsIgnoreCase("discover"))
-                                              {
-                                                  System.out.println(Style.apply(message, "yellow"));
-                                              }
-                                          });
+            client.start();
 
-                client.start();
-
-                Threads.get().schedule(() ->
+            Threads.get().schedule(() ->
+                                   {
+                                       try
                                        {
-                                           try
-                                           {
-                                               client.send("Discover");
-                                           }
-                                           catch (IOException e)
-                                           {
-                                               System.err.println(Style.apply(e));
-                                           }
+                                           client.send("Discover");
+                                       }
+                                       catch (IOException e)
+                                       {
+                                           System.err.println(Style.apply(e));
+                                       }
 
-                                       }, 1, TimeUnit.SECONDS);
+                                   }, 1, TimeUnit.SECONDS);
 
-                Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
 
-                sc.nextLine();
-                sc.close();
+            sc.nextLine();
+            sc.close();
+
+            System.exit(0);
+        }
+        else
+        {
+            int port = 0;
+
+            if (parser.wasExecuted("port"))
+            {
+                port = Integer.parseInt(portCmd.getValue());
+
+                if (serverCmd.getFlag())
+                {
+                    new BtServerConsole(port);
+                }
+                else
+                {
+                    String host = hostCmd.getValue() != null ? hostCmd.getValue() : "localhost";
+                    new BtClientConsole(host, port, !rawCmd.isExecuted());
+                }
 
                 System.exit(0);
             }
             else
             {
-                int port = 0;
-
-                if (parser.wasExecuted("port"))
-                {
-                    port = Integer.parseInt(portCmd.getValue());
-
-                    if (serverCmd.getFlag())
-                    {
-                        new BtServerConsole(port);
-                    }
-                    else
-                    {
-                        String host = hostCmd.getValue() != null ? hostCmd.getValue() : "localhost";
-                        new BtClientConsole(host, port, !rawCmd.isExecuted());
-                    }
-
-                    System.exit(0);
-                }
-                else
-                {
-                    System.out.println(Style.apply("Usage: btc [-host <hostname>] -port <portnumber> [-raw] [-server]", "red"));
-                }
+                System.err.println("Usage: btc [-host <hostname>] -port <portnumber> [-raw] [-server]");
             }
-        }
-        catch (Exception e)
-        {
-            System.err.println(Style.apply(e));
-            System.exit(-1);
         }
     }
 
